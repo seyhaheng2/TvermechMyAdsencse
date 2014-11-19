37bd61ffaee3fe1583d91c82d1a70086340f6b33
seyhaheng2 <seyhaheng2@gmail.com>
2014-11-19 18:31:42 +0700

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,9 @@ Category.create({name: "Technology"})
 Category.create({name: "Life"})
 Category.create({name: "Education"})
 Category.create({name: "Others"})
+
+10.times do |i|
+	Post.create(name: "Learn Rials#{i}", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus rem nihil iusto, eos, officiis itaque repellendus quaerat alias esse ipsa perspiciatis ipsam, deleniti libero minima magni neque consequatur. Dolore, provident.", category_id: "#{i}", image: "#{i}.png")
+end
+
+
